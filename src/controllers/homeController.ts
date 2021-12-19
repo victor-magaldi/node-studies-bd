6dc4d6cc6a30fd6538fac6dfc7b2a7bcb37c91a5
victor de souza magaldi <victmagaldi@hotmail.com>
2021-12-19 15:43:42 -0300

    testing create e delete

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,12 +1,45 @@
 import { Request, Response } from 'express';
+import { title } from 'process';
+import { Op } from 'sequelize';
 import { sequelize } from '../instances/mysql';
 
 import { Product } from '../models/Product';
 import { User } from '../models/user';
 
 export const home = async (req: Request, res: Response) => {
-    const users = await User.findAll();
-    console.log(users.every(user => user instanceof User)); // true
+    try {
+        // --- add data   
+        // const newUser = await User.build({ name: "Sequelize build", age: 99 });
+        // newUser.save()
+        // console.log("newUser ADD ", newUser)
+
+
+        // ---  remove data
+        // await User.destroy({
+        //     where: {
+        //         id: 10
+        //     }
+        // })
+        // console.log("deletado")
+
+        await User.u({
+            where: {
+                id: 10
+            }
+        })
+        console.log("deletado")
+
+    }
+    catch (err) { console.log(err); }
+
+    const users = await User.findAll({
+        where: {
+            id: {
+                [Op.eq]: 4
+            }
+        }
+    });
+
     console.log("All users:", JSON.stringify(users, null, 2));
     try {
         await sequelize.authenticate()
