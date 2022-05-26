c145fba78f5160be0fb785d5b40fecdf10e86cef
victor de souza magaldi <victmagaldi@hotmail.com>
2022-05-26 19:10:02 -0300

    test

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,19 +1,16 @@
-import { Request, Response } from 'express';
-import { title } from 'process';
-import { Op } from 'sequelize';
-import { sequelize } from '../instances/mysql';
+import { Request, Response } from "express";
+import { Op } from "sequelize";
+import { sequelize } from "../instances/mysql";
 
-import { Product } from '../models/Product';
-import { User } from '../models/user';
+import { Product } from "../models/Product";
+import { User } from "../models/user";
 
 export const home = async (req: Request, res: Response) => {
     try {
-        // --- add data   
+        // --- add data
         // const newUser = await User.build({ name: "Sequelize build", age: 99 });
         // newUser.save()
         // console.log("newUser ADD ", newUser)
-
-
         // ---  remove data
         // await User.destroy({
         //     where: {
@@ -21,31 +18,30 @@ export const home = async (req: Request, res: Response) => {
         //     }
         // })
         // console.log("deletado")
-
-        await User.u({
-            where: {
-                id: 10
-            }
-        })
-        console.log("deletado")
-
+        // await User.update({
+        //     where: {
+        //         id: 10,
+        //     },
+        // });
+        // console.log("deletado");
+    } catch (err) {
+        console.log(err);
     }
-    catch (err) { console.log(err); }
 
     const users = await User.findAll({
         where: {
             id: {
-                [Op.eq]: 4
-            }
-        }
+                [Op.eq]: 4,
+            },
+        },
     });
 
     console.log("All users:", JSON.stringify(users, null, 2));
     try {
-        await sequelize.authenticate()
-        console.log("conexao")
+        await sequelize.authenticate();
+        console.log("conexao");
     } catch (e) {
-        console.log("error BD", e)
+        console.log("error BD", e);
     }
     let age: number = 90;
     let showOld: boolean = false;
@@ -57,12 +53,12 @@ export const home = async (req: Request, res: Response) => {
     let list = Product.getAll();
     let expensiveList = Product.getFromPriceAfter(12);
 
-    res.render('pages/home', {
-        name: 'Bonieky',
-        lastName: 'Lacerda',
+    res.render("pages/home", {
+        name: "Bonieky",
+        lastName: "Lacerda",
         showOld,
         products: list,
         expensives: expensiveList,
-        frasesDoDia: []
+        frasesDoDia: [],
     });
 };
